8b3e940babfbef47c827d7135c4564436cc766d4
Gethe <gethe88@gmail.com>
2015-01-01 16:13:44 -0600

    poke

--- itIT.lua
+++ itIT.lua
@@ -3,9 +3,9 @@ local L = LibStub("AceLocale-3.0"):NewLocale("nibRealUI", "itIT")
 if L then
 
 -- General
---@localization(locale="itIT", format="lua_additive_table")@
+--@localization(locale="itIT", format="lua_additive_table", handle-subnamespaces="none")@
 
 -- InfoLine
---@localization(locale="itIT", format="lua_additive_table" namespace="Infoline")@
+--@localization(locale="itIT", format="lua_additive_table", namespace="Infoline")@
 
 end
