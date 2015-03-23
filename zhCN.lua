30b54fe14c00fe5368f185e20cde1862d0178ccb
Gethe <gethe88@gmail.com>
2015-03-23 09:55:11 -0500

    add ruRU and zhCN

---
+++ zhCN.lua
@@ -0,0 +1,19 @@
+﻿local L = LibStub("AceLocale-3.0"):NewLocale("nibRealUI", "deDE")
+
+if L then
+
+-- General
+--@localization(locale="deDE", format="lua_additive_table", namespace="General")@
+
+-- Install
+--@localization(locale="deDE", format="lua_additive_table", namespace="Install")@
+
+-- Options
+--@localization(locale="deDE", format="lua_additive_table", namespace="Options")@
+
+-- Config
+--@localization(locale="deDE", format="lua_additive_table", namespace="ConfigBar")@
+
+-- InfoLine
+--@localization(locale="deDE", format="lua_additive_table", namespace="InfoLine")@
+end
